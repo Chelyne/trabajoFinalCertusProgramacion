c1e26c087ec104ac44a9a051e828a970a85ee7ce
celine <canariancco@gmail.com>
2022-11-07 22:28:46 -0500

    trabajo final de introducion a la programacion

---
+++ viajes.js
@@ -0,0 +1,5 @@
+function mostrarAlerta(){
+    var nombre = document.getElementById("nombre").value;
+    console.log("nombre", nombre)
+    alert('Hola '+ nombre +' , tus datos fueron ingresados  Correctamente.');
+}
